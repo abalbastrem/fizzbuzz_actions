554de02fa60a96d63e434d4aa2cd213e76e71d24
abalbastrem <albert.balbastre@gmail.com>
2024-02-19 19:53:30 +0100

    tests pass

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -24,16 +24,16 @@ func TestFizzbuzzSuccessStandardNumbers(t *testing.T) {
 		buzz: 5,
 	})
 
-	assert.Equal(t, res[0], 1)
-	assert.Equal(t, res[1], 2)
-	assert.Equal(t, res[2], "fizz")
-	assert.Equal(t, res[3], 4)
-	assert.Equal(t, res[4], "buzz")
-	assert.Equal(t, res[5], 6)
-	assert.Equal(t, res[14], "fizzbuzz")
-	assert.Equal(t, res[29], "fizz")
-	assert.Equal(t, res[99], "buzz")
-	assert.Equal(t, res[299], "fizzbuzz")
+	assert.Equal(t, "1", res[0])
+	assert.Equal(t, "2", res[1])
+	assert.Equal(t, "fizz", res[2])
+	assert.Equal(t, "4", res[3])
+	assert.Equal(t, "buzz", res[4])
+	assert.Equal(t, "fizz", res[5])
+	assert.Equal(t, "fizzbuzz", res[14])
+	assert.Equal(t, "fizzbuzz", res[29])
+	assert.Equal(t, "buzz", res[99])
+	assert.Equal(t, "fizzbuzz", res[299])
 
 	assert.NoError(t, err)
 }
@@ -44,13 +44,13 @@ func TestFizzbuzzSuccessHighNumbers(t *testing.T) {
 		buzz: 250,
 	})
 
-	assert.Equal(t, res[0], 1)
-	assert.Equal(t, res[1], 2)
-	assert.Equal(t, res[2], 3)
-	assert.Equal(t, res[99], "fizz")
-	assert.Equal(t, res[249], "buzz")
-	assert.Equal(t, res[2499], "fizzbuzz")
-	assert.Equal(t, res[2500], 2501)
+	assert.Equal(t, "1", res[0])
+	assert.Equal(t, "2", res[1])
+	assert.Equal(t, "3", res[2])
+	assert.Equal(t, "fizz", res[99])
+	assert.Equal(t, "buzz", res[249])
+	assert.Equal(t, "fizzbuzz", res[2499])
+	assert.Equal(t, "2501", res[2500])
 
 	assert.NoError(t, err)
 }
@@ -61,16 +61,17 @@ func TestFizzbuzzSuccessLowNumbers(t *testing.T) {
 		buzz: 3,
 	})
 
-	assert.Equal(t, res[0], 1)
+	assert.Equal(t, res[0], "1")
 	assert.Equal(t, res[1], "fizz")
 	assert.Equal(t, res[2], "buzz")
 	assert.Equal(t, res[3], "fizz")
-	assert.Equal(t, res[4], 5)
+	assert.Equal(t, res[4], "5")
 	assert.Equal(t, res[5], "fizzbuzz")
-	assert.Equal(t, res[6], 7)
+	assert.Equal(t, res[6], "7")
 	assert.Equal(t, res[7], "fizz")
 	assert.Equal(t, res[199], "fizz")
-	assert.Equal(t, res[299], "buzz")
+	assert.Equal(t, res[299], "fizzbuzz")
+	assert.Equal(t, res[332], "buzz")
 	assert.Equal(t, res[599], "fizzbuzz")
 
 	assert.NoError(t, err)
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 var (
 	errNegativeNumber = errors.New("ERROR: no zero nor negative numbers allowed")
+)
+
+const (
+	fizzbuzzN = 5000
 )
 
 func main() {
@@ -19,7 +24,24 @@ type RuleSet struct {
 }
 
 func fizzbuzz(ruleset RuleSet) ([]string, error) {
-	var fizzbuzz []string
-	// TODO
-	return fizzbuzz, errNegativeNumber
+	var fizzbuzzes []string
+
+	if ruleset.fizz <= 0 || ruleset.buzz <= 0 {
+		return []string{}, errNegativeNumber
+	}
+	for i := 1; i <= fizzbuzzN; i++ {
+		var result string
+		if i%ruleset.fizz != 0 && i%ruleset.buzz != 0 {
+			result += strconv.Itoa(i)
+		}
+		if i%ruleset.fizz == 0 {
+			result += "fizz"
+		}
+		if i%ruleset.buzz == 0 {
+			result += "buzz"
+		}
+		fizzbuzzes = append(fizzbuzzes, result)
+	}
+
+	return fizzbuzzes, nil
 }
